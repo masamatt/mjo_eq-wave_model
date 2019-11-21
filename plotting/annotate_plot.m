351f6b1f846ea874160ec89c5a169765314f5129
Matt Masarik <matt.masarik@gmail.com>
2019-11-21 13:47:56 -0700

    removed: annotate_plot.m.  moved to, label_plot.m.

--- plotting/annotate_plot.m
+++
@@ -1,7 +0,0 @@
-function label_plot(title_str)
-    %
-    %
-    title(title_str,'fontsize',10,'interpreter','latex');
-    xlabel('$\xi\,\,(10^{3}\,$km)','fontsize',8,'interpreter','latex');
-    ylabel('$y\,\,(10^{3}\,$km)','fontsize',8,'interpreter','latex');
-end
