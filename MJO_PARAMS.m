a9437cf89211451479253fb029383ce0e382f0f6
Matt Masarik <matt.masarik@gmail.com>
2019-11-25 17:42:59 -0700

    MJO_PARAMS.m + MJO_PLOT.m - all parameters set to default values.

--- MJO_PARAMS.m
+++ MJO_PARAMS.m
@@ -5,7 +5,7 @@
 
 % *****************  DIABATIC FORCING PARAMETERS ****************** %
 % Q0/cp [K/day]:  diabatic heating rate          [default, Q0/cp=12K/day]
-Q0_cp = 15;                      %   comment: for fixed integrated heating
+Q0_cp = 12;                      %   comment: for fixed integrated heating
 fixedIntegratedForcing=false;    %   comment: for fixed integrated heating
 %%% fixedIntegratedForcing=true; % uncomment: for fixed integrated heating
 %%% Q0_cp     = NaN;             % uncomment: for fixed integrated heating
@@ -17,15 +17,15 @@ fixedIntegratedForcing=false;    %   comment: for fixed integrated heating
 
 % a0 [km]:  zonal half-width of diabatic heating
 %           [default, a0=1250km]
-a0_km = 300;
+a0_km = 1250;
 
 % b0 [km]:  meridional e-folding width of diabatic heating
 %           [default,  b0=450km]
-b0_km = 350;
+b0_km = 450;
 
 % y0 [km]:  meridional displacement from equator of diabatic heating
 %           [default,  y0=450km]
-y0_km = 1200;        
+y0_km = 450;        
 
 % c [m/s]:  phase speed of diabatic forcing
 %           [default,    c=5m/s]
@@ -38,10 +38,10 @@ p = 850;
 
 
 % N []:  maximum meridional mode, integer            [default,    N=200]
-nMax = 300;
+nMax = 200;
 
 % M []:  maximum zonal wavenumber, integer           [default,    M=200]
-mMax = 300;
+mMax = 200;
 
 
 % zonalDomain []:  zonal extent of equatorial channel to display
--- MJO_PLOT.m
+++ MJO_PLOT.m
@@ -10,33 +10,33 @@
 % ***************** USER PLOT PARAMETERS *************************** %
 % GENERAL
 % =======
-overlayEquator        = false;
-overlayForcing        = false;
-displayColorBar       = true;
-displayPeakValues     = true;
+overlayEquator        = false;     %                 [default, false]
+overlayForcing        = false;     %                 [default, false]
+displayColorBar       = true;      %                 [default,  true]
+displayPeakValues     = true;      %                 [default,  true]
 
-saveFigHardcopy       = false;
-saveFigType           = 'pdf';    % pdf, eps, png
+saveFigHardcopy       = false;     %                 [default, false]
+saveFigType           = 'pdf';     % pdf, eps, png   [default,   pdf]
 
 
 % [PANEL 1]:  POTENTIAL VORTICITY
 % ===============================
-newPLevelPVFlag       = false;
-newPLevelPVValue      = 500;      % hPa
+newPLevelPVFlag       = false;     %                 [default, false]
+newPLevelPVValue      = 500;       % hPa
 
 
 % [PANEL 2]:  U V PHI PLOT
 % ========================
-newPLevelUVPhiFlag    = false;
-newPLevelUVPhiValue   = 500;      % hPa
+newPLevelUVPhiFlag    = false;     %                 [default, false]
+newPLevelUVPhiValue   = 500;       % hPa
 
-vectorDensityStride   = 5;       % [integer, default=5]
-vectorScaleFactor     = 0.9;     % [float, default=0.9]
+vectorDensityStride   = 5;         % (integer)       [default,     5]
+vectorScaleFactor     = 0.9;       % (float)         [default,   0.9]
 
 
 % [PANEL 3]:  OMEGAM
 % ==================
-newPLevelOmegaMFlag    = false;
+newPLevelOmegaMFlag    = false;    %                 [default, false]
 newPLevelOmegaMValue   = 500;      % hPa
 % *********************** END PARAMETERS *************************** %
 
