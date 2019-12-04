cd57f8b0aff47b9329e2b142b9ee415d18c3fcd0
Matt Masarik <matt.masarik@gmail.com>
2019-12-04 15:17:18 -0700

    Added user adjustable forcing contour levels for plotting.

--- plotting/contour_DiabForcing.m
+++ plotting/contour_DiabForcing.m
@@ -1,14 +1,17 @@
-function contour_DiabForcing(XI,Y,Qdf)
+function contour_DiabForcing(XI,Y,Q_field,Q_levs)
 %
 %
-% plot_Q.m - plot contours of Q (diabatic heating)
-%
+% plot_Q.m - plot contours of Q_fields (diabatic heating), specified
+%            by vector Q_levs.
 %
 
-Qmax = max(max(Qdf));
+% Qmax = max(max(Q_field));
+% Q_levs = [0.5*Qmax 0.0625*Qmax];
 
-contour(XI,Y,Qdf, ...
+
+
+contour(XI,Y,Q_field, ...
     'LineStyle',':','LineColor','k','LineWidth',1.75, ...
-    'LevelListMode','manual','LevelList',[0.5*Qmax 0.0625*Qmax]);
+    'LevelListMode','manual','LevelList',Q_levs);
 
 end
